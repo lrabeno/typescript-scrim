b6791de7911fcb17c286f462de47648a5a3be2ef
Louis J Rabeno <lrabeno@gmail.com>
2025-01-13 15:53:42 -0500

    added omit type to addNewPizza function

--- index.ts
+++ index.ts
@@ -10,6 +10,10 @@ type Order = {
   status: 'ordered' | 'completed';
 };
 
+let cashInRegister = 100;
+let nextOrderId = 1;
+let nextPizzaId = 1;
+
 /* 
 to make a property optional put a question mark before the colon like in name below
  type Pizza = {
@@ -19,22 +23,30 @@ to make a property optional put a question mark before the colon like in name be
 */
 
 const menu: Array<Pizza> = [
-  { id: 1, name: 'Margherita', price: 8 },
-  { id: 2, name: 'Pepperoni', price: 10 },
-  { id: 3, name: 'Hawaiian', price: 10 },
-  { id: 4, name: 'Veggie', price: 9 },
+  { id: nextPizzaId++, name: 'Margherita', price: 8 },
+  { id: nextPizzaId++, name: 'Pepperoni', price: 10 },
+  { id: nextPizzaId++, name: 'Hawaiian', price: 10 },
+  { id: nextPizzaId++, name: 'Veggie', price: 9 },
 ];
 
-let cashInRegister = 100;
-let nextOrderId = 1;
 const orderQueue: Array<Order> = [];
 
 // can also define arrays like below
 // const orderQueue: Order[] = []
 
-function addNewPizza(pizzaObj: Pizza): void {
-  menu.push(pizzaObj);
+function addNewPizza(pizzaObj: Omit<Pizza, 'id'>): Pizza {
+  // Create a newPizzaObj so we can call addNewPizza with the id omitted
+  const newPizzaObj: Pizza = {
+    id: nextPizzaId++,
+    ...pizzaObj,
+  };
+  menu.push(newPizzaObj);
+  return newPizzaObj;
 }
+
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+addNewPizza({ name: 'BBQ Chicken', price: 12 });
+addNewPizza({ name: 'Spicy Sausage', price: 11 });
 
 function placeOrder(pizzaName: string): Order | undefined {
   const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
@@ -63,7 +75,7 @@ function completeOrder(orderId: number): Order | undefined {
   return order;
 }
 
-export function getPizzaDetail(identifier: string | number): Pizza | undefined {
+function getPizzaDetail(identifier: string | number): Pizza | undefined {
   if (typeof identifier === 'string') {
     return menu.find(
       (pizza) => pizza.name.toLowerCase() === identifier.toLowerCase()
@@ -76,10 +88,6 @@ export function getPizzaDetail(identifier: string | number): Pizza | undefined {
     );
   }
 }
-
-addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
-addNewPizza({ name: 'BBQ Chicken', price: 12 });
-addNewPizza({ name: 'Spicy Sausage', price: 11 });
 
 placeOrder('Chicken Bacon Ranch');
 completeOrder(1);
